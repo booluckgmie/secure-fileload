83d938b6a15d8bb08b813ea2f327e3ec0f77c1be
Najmi Ariffin <booluckgmie@gmail.com>
2025-08-20 17:39:01 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const API_BASE =
   import.meta.env.VITE_API_BASE ||
   (import.meta.env.DEV
-    ? "http://localhost:8888/.netlify/functions/api"
+    ? "https://nexaflowdb.netlify.app/.netlify/functions/api"
     : "/.netlify/functions/api");
 
 export { API_BASE };
